dc25812101d5aec2c2c666d2bb5fa367df727389
Bohdan <dogdanlashko@gmail.com>
2024-07-21 22:40:45 +0200

    home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,29 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import Form from './Form.svelte';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Url Shortener</title>
+	<meta name="description" content="Url shortener service" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<p>Hello world!</p>
-
 	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
+		Enter your <strong>URL</strong> and preferred shortening for it:
 	</h2>
 
-	<Counter />
+	<p>A new link will be generated and will be pointing to your original url.</p>
+
+	<Form />
 </section>
 
-<style>
+<style lang="scss">
 	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+		text-align: center;
 	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	h2 {
+		margin-top: 3em;
+		// padding: 1em;
 	}
 </style>
