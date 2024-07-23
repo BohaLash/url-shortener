6d170498664d3573b8c28762a2d609e351cf1c29
Bohdan <dogdanlashko@gmail.com>
2024-07-23 21:18:19 +0200

    fix load errors

--- src/routes/[shortUrl]/stats/+page.server.ts
+++ src/routes/[shortUrl]/stats/+page.server.ts
@@ -1,4 +1,4 @@
-import { fail } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 
@@ -6,25 +6,24 @@ const PAGE_LIMIT = 10;
 
 
 export const load = (async ({ platform, params }) => {
-
 	const URL_KV = platform?.env?.URL_KV;
 
-	if (!URL_KV) return fail(500, { message: 'something went wrong' });
+	if (!URL_KV) error(500, { message: 'something went wrong' });
 
-	const url = await URL_KV.get('/' + params.shortUrl);
+	const url: string = await URL_KV.get('/' + params.shortUrl);
 
-	if (!url) return fail(404, { message: 'URL not found' });
+	if (!url) error(404, { message: 'URL not found' });
 
 	const totals = await URL_KV.get('totals/' + params.shortUrl);
 
 	const clicks = await URL_KV.list({ prefix: params.shortUrl + '/', limit: PAGE_LIMIT });
 
-	if (!clicks || !clicks.keys) return fail(500, { message: 'something went wrong' });
+	if (!clicks || !clicks.keys) error(500, { message: 'something went wrong' });
 
 	return {
 		url,
 		shortUrl: params.shortUrl,
-		totals: parseInt(totals) || 0, // TODO: fix
+		totals: parseInt(totals) || 0,
 		clicks: clicks.keys.map((click: { name: string, metadata: any }) => click.metadata),
 		next: !clicks.list_complete && clicks.cursor,
 	};
--- src/routes/list/+page.server.ts
+++ src/routes/list/+page.server.ts
@@ -1,4 +1,4 @@
-import { fail } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 
@@ -9,11 +9,11 @@ export const load = (async ({ platform }) => {
 
 	const URL_KV = platform?.env?.URL_KV;
 
-	if (!URL_KV) return fail(500, { message: 'something went wrong' });
+	if (!URL_KV) error(500, { message: 'something went wrong' });
 
 	const links = await URL_KV.list({ prefix: '/', limit: PAGE_LIMIT });
 
-	if (!links || !links.keys) return fail(500, { message: 'something went wrong' });
+	if (!links || !links.keys) error(500, { message: 'something went wrong' });
 
 	return {
 		links: links.keys.map((link: { name: string }) => link.name),
